2670bbe16bc3bae2854cc3391a4b49bd3d9ffa1a
jimena <jimena.mit@gmail.com>
2014-04-06 22:18:49 -0400

    adding some css to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Pinteresting::Application.routes.draw do
 root "pages#home"  
-get "home" => "pages#home"
 get "about" => "pages#about"
 # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
